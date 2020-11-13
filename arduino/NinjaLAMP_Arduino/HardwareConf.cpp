a7a5c4934af5c6c637665395619a74ce96f2f305
Mariko Hisakawa <avenafatua@gmail.com>
2020-11-13 13:28:33 +0900

    Define NXFT15WF104FA2B100 thermistor

--- arduino/NinjaLAMP_Arduino/HardwareConf.cpp
+++ arduino/NinjaLAMP_Arduino/HardwareConf.cpp
@@ -35,29 +35,24 @@ const int AIR_THERMISTOR_AIN = A1;
 ADCArduino adc(WELL_THERMISTOR_AIN, AIR_THERMISTOR_AIN);
 
 /* Well */
-struct ThermistorRange wellThermistorRanges[3] = {
+struct ThermistorRange NXFT15WF104FA2B100[3] = {
   { 0.0, 4250, 0.0, }, /* 4250 for 0-50 deg */
   { 50.0, 4311, 0.0, }, /* 4311 for 50-85 deg */
   { 85.0, 4334, 0.0, } /* 4334 for 85-100 deg */
 };
+
 Thermistor wellThermistor = { 
   .bConstRangeCount = 3, /* Number of B-constant ranges */
-  .bConstRanges = wellThermistorRanges, /* Pointer to the B-constant ranges array */
+  .bConstRanges = NXFT15WF104FA2B100, /* Pointer to the B-constant ranges array */
   .r0 = 100.0, /* Resistance at "baseTemp" (kOhm) */
   .baseTemp = 25.0,
   .place = THERMISTOR_LOW_SIDE,
   .useSwitching = false,
   .r = 47.0 /* kOhm */
 };
-/* Air */
-struct ThermistorRange airThermistorRanges[3] = {
-  { 0.0, 4250, 0.0, }, /* 4250 for 0-50 deg */
-  { 50.0, 4311, 0.0, }, /* 4311 for 50-85 deg */
-  { 85.0, 4334, 0.0, } /* 4334 for 85-100 deg */
-};
 Thermistor airThermistor = { 
   .bConstRangeCount = 3, /* Number of B-constant ranges */
-  .bConstRanges = airThermistorRanges, /* Pointer to the B-constant ranges array */
+  .bConstRanges = NXFT15WF104FA2B100, /* Pointer to the B-constant ranges array */
   .r0 = 100.0, /* Resistance at "baseTemp" (kOhm) */
   .baseTemp = 25.0,
   .place = THERMISTOR_LOW_SIDE,
@@ -72,18 +67,6 @@ NinjaLAMPCore * HardwareConf::initFirstModel () {
     WELL_KP, WELL_KI, WELL_KD,
     WELL_HEATER_PWM
   );
-  /*
-  NinjaLAMPCore core = {
-    .wellThermistorConf = &wellThermistor, 
-    .airThermistorConf = &airThermistor,
-    .adc = &adc,
-    .wellKP = WELL_KP,
-    .wellKI = WELL_KI,
-    .wellKD = WELL_KD,
-    .heaterPWM = WELL_HEATER_PWM
-  };
-  */
-  
   return core;
 }
 NinjaLAMPCore * HardwareConf::initLCDModel () {
